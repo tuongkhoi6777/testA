f2c770ace4cd061436e86185689c6ed2bd5704c1
tuongkhoi6777 <tuongkhoi6777@gmail.com>
2021-09-10 17:28:55 +0700

    first commit

---
+++ src/settings.c8241.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/game.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"45e63",main:"ca0c8"}};
